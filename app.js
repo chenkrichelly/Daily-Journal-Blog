53c0e3295739db3faf45e7cb87f80b36224f4af8
Chen Krichelly <112880311+chenkrichelly@users.noreply.github.com>
2023-05-10 21:39:56 +0300

    updated database and stylized code

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const ejs = require("ejs");
 const { forEach } = require("lodash");
 var _ = require("lodash");
+const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+
+const homeStartingContent = "This blog is your personal space where you can document your daily thoughts, experiences, and reflections. By subscribing to this blog, you have taken the first step towards creating a daily journaling practice. Through this blog, we will explore a range of topics, including mindfulness, productivity, relationships, and more.My hope is that this blog will inspire and motivate you to take some time out of your day to reflect on your experiences and emotions. Journaling is an incredibly powerful tool for self - discovery and personal growth.It allows you to develop a deeper understanding of yourself and the world around you.By regularly documenting your thoughts and experiences, you can gain insights into your patterns of behavior and thought, identify areas of your life that need attention, and develop a greater sense of clarity and purpose. Thank you for subscribing to this blog and embarking on this journey of self - discovery with me. I look forward to sharing daily insights and musings with you!";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -16,10 +18,23 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var posts = [];
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
 
-app.get("/", (req, res) => {
-  res.render("home", { homeContent: homeStartingContent, posts:posts });
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function (req, res) {
+
+  Post.find().then(posts => {
+    res.render("home", {
+      homeContent: homeStartingContent,
+      posts: posts
+    });
+  });
 });
 
 app.get("/about", (req, res) => {
@@ -33,32 +48,35 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose", (req, res) => {
-  var post = {
+app.post("/compose", async (req, res) => {
+ 
+  const post = new Post({
     title: req.body.titleBox,
-    slug: _.kebabCase(req.body.titleBox),
     content: req.body.postBox
-  };
+  });
 
-  posts.push(post);
-  res.redirect("/");
+  try {
+    await post.save();
+    res.redirect("/");
+  } catch (err) {
+    // Handle error
+  }
 
 });
 
+app.get("/posts/:postId", async (req, res) => {
+  const requestedPostId = req.params.postId;
 
-
-app.get("/posts/:postName", (req, res) => {
-  const request = _.lowerCase(req.params.postName);
-  posts.forEach(function (post) {
-    if (_.lowerCase(post.title) === request) {
-      console.log("Match found!");
-      res.render("post", { title: post.title, content: post.content })
-    }else{
-      console.log("Match not found!");
-    }
-  })
-  
-
+  try {
+    const post = await Post.findOne({ _id: requestedPostId });
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 });
 
 
